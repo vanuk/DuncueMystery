252e6268bb1be4af302e4d4044bd5f49cf8a22c5
vano <sinkovvv1@gmail.com>
2022-09-02 19:56:17 +0300

    push

---
+++ Assets/po1.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class po1 : MonoBehaviour
+
+{
+    public int currentLife;
+    public GameObject spawn;
+    public GameObject checkpoint;
+    public GameObject checkpoint2;
+    private bool _checkpointActivated = false;
+   
+    void Start()
+    {
+        currentLife = 3;
+        transform.position = spawn.transform.position;
+    }
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("DeathCollider1"))
+        {
+            currentLife--;
+            if (!_checkpointActivated) {
+                transform.position = spawn.transform.position;
+            }
+            else {
+                transform.position = checkpoint.transform.position;
+            }
+           
+        }
+
+        if (col.gameObject.CompareTag("Checkpoint1"))
+        {
+            _checkpointActivated = true;
+        }
+       
+    }
+
+
+}
